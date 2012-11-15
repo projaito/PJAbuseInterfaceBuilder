12458152163569d1de0bad70e0686ed9894ea63b
James Tang <mystcolor@gmail.com>
2012-11-15 22:59:56 +0800

    - Use Storyboard for XIBFlowLayoutDemo

--- XIBFlowLayoutViewDemo/AppDelegate.m
+++ XIBFlowLayoutViewDemo/AppDelegate.m
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    /*
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
@@ -23,6 +24,7 @@
     }
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+     */
     return YES;
 }
 
